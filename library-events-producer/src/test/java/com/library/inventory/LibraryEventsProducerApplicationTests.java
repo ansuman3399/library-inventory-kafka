0636e02202f6fe42494d0dc8dc16aed15af3adf0
Ansuman Mohanty <ansumanm20@gmail.com>
2022-03-25 20:36:17 +0530

    Added library-events-producer integration test implementation

--- library-events-producer/src/test/java/com/library/inventory/LibraryEventsProducerApplicationTests.java
+++ library-events-producer/src/test/java/com/library/inventory/LibraryEventsProducerApplicationTests.java
@@ -1,13 +1,85 @@
 package com.library.inventory;
 
-import org.junit.jupiter.api.Test;
+import com.library.inventory.domain.Book;
+import com.library.inventory.domain.LibraryEvent;
+import com.library.inventory.domain.LibraryEventType;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//including embedded kafka so that the test can run without having our local kafka server up.
+@EmbeddedKafka(topics = {"library-events"}, partitions = 3)
+//pick up the brokers value dynamically from EmbeddedKafkaBroker.java
+@TestPropertySource(properties = {"spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}",
+        "spring.kafka.admin.properties.bootstrap.servers=${spring.embedded.kafka.brokers}"})
+@ExtendWith(SpringExtension.class)
 class LibraryEventsProducerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
+    private Consumer<Integer, String> consumer;
+
+    @Autowired
+    EmbeddedKafkaBroker embeddedKafkaBroker;
+
+    @BeforeEach
+    void setUp() {
+        Map<String, Object> configs = new HashMap<>(KafkaTestUtils.consumerProps("group1", "true", embeddedKafkaBroker));
+        consumer = new DefaultKafkaConsumerFactory<>(configs, new IntegerDeserializer(), new StringDeserializer()).createConsumer();
+        embeddedKafkaBroker.consumeFromAllEmbeddedTopics(consumer);
+    }
+
+    @AfterEach
+    void tearDown() {
+        consumer.close();
+    }
+
+    @Test
+    @Timeout(5)
+    void postLibraryEvent() throws InterruptedException {
+        //given
+        LibraryEvent libraryEvent = new LibraryEvent().builder()
+                .libraryEventId(1)
+                .book(new Book().builder().bookId(1).bookName("RDPD").author("RK").build())
+                .libraryEventType(LibraryEventType.NEW)
+                .build();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("content-type", MediaType.APPLICATION_JSON_VALUE.toString());
+        HttpEntity<LibraryEvent> request = new HttpEntity<>(libraryEvent, headers);
+
+        //when
+        ResponseEntity<LibraryEvent> exchange = testRestTemplate.exchange("/v1/libraryevent", HttpMethod.POST, request, LibraryEvent.class);
+
+        //then
+        assertEquals(HttpStatus.CREATED, exchange.getStatusCode());
+
+        ConsumerRecord<Integer, String> singleRecord = KafkaTestUtils.getSingleRecord(consumer, "library-events");
+
+        Thread.sleep(3000);
+
+        String value = singleRecord.value();
+        String expected="{\"libraryEventId\":1,\"libraryEventType\":\"NEW\",\"book\":{\"bookId\":1,\"bookName\":\"RDPD\",\"author\":\"RK\"}}";
+        assertEquals(expected, value);
+    }
 
 }
